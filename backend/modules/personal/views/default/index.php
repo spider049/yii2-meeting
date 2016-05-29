7921393c4ec64fe3111603b6f97a728766d23309
Mywin10pro049 <Mywin10pro049@DESKTOP-67PRJ0K>
2016-05-29 21:31:55 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- backend/modules/personal/views/default/index.php
+++ backend/modules/personal/views/default/index.php
@@ -1,5 +1,6 @@
 <div class="personal-default-index">
     <h1><?= $this->context->action->uniqueId ?></h1>
+    <h2>หน้าแรกของ Module Personal</h2>
     <p>
         This is the view content for action "<?= $this->context->action->id ?>".
         The action belongs to the controller "<?= get_class($this->context) ?>"
